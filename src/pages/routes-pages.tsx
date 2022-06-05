bc1adb5a1aaed2cd45c4f47b2a0944ca774fff1d
SteveJamerson <stevejamerson@outlook.com>
2022-06-05 13:08:52 -0300

    feat: created home

--- src/pages/routes-pages.tsx
+++ src/pages/routes-pages.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
 import NotFound from "./NotFound";
 
 export const RoutesPages = () => {
    return (
       <Routes>
+         <Route path="/" element={<Home />} />
          <Route path="*" element={<NotFound />} />
       </Routes>
    );
