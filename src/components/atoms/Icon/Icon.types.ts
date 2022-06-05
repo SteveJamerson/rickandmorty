43fbad9103680c478193ff4f9c8f0a0a22a4d791
SteveJamerson <stevejamerson@outlook.com>
2022-06-05 20:18:04 -0300

    feat: created component icon

---
+++ src/components/atoms/Icon/Icon.types.ts
@@ -0,0 +1,11 @@
+import { ReactComponent as ArrowDown } from './svg/arrow-down.svg';
+
+const IconsTypes = {
+   'arrow-down': ArrowDown,
+};
+
+export default IconsTypes;
+
+export type IconName = 'arrow-down';
+
+export type IconPosition = 'start' | 'end' | 'up' | 'down';
