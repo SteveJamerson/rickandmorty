024ae4ac54eabdf2e54b87639abe890646cdadc4
SteveJamerson <stevejamerson@outlook.com>
2022-06-06 01:10:36 -0300

    feat: updated icon

--- src/components/atoms/Icon/Icon.types.ts
+++ src/components/atoms/Icon/Icon.types.ts
@@ -1,11 +1,13 @@
 import { ReactComponent as ArrowDown } from './svg/arrow-down.svg';
+import { ReactComponent as Search } from './svg/search.svg';
 
 const IconsTypes = {
    'arrow-down': ArrowDown,
+   search: Search,
 };
 
 export default IconsTypes;
 
-export type IconName = 'arrow-down';
+export type IconName = 'arrow-down' | 'search';
 
 export type IconPosition = 'start' | 'end' | 'up' | 'down';
--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,4 +1,6 @@
 export * from "./Button";
 export * from "./Dropdown";
+export * from "./Field";
 export * from "./Icon";
 export * from "./Image";
+export * from "./Select";
